713089bcc14194b7238beb31999809005661d526
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-12 18:21:49 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Getting references to the inputs and logo
 let userInputElement = document.getElementById("phone-number");
 let feedbackElement = document.getElementById("feedback");
 let networkLogo = document.getElementById("networkLogo");
@@ -9,14 +10,22 @@ let submit = document.getElementById("submit");
 userInputElement.oninput = function () {
   let phoneNum = userInputElement.value;
 
+  //Gets first four digits of normal number
   let numCode = phoneNum.substr(0, 4);
+  //Gets first seven digits for +234 numbers
   let intNumCode = phoneNum.substr(0, 7);
 
+  //Replace +234 with zero
   let newIntNumCode = intNumCode.replace("+234", "0");
 
+  //tracking in an a network provider is selected
   let selected = false;
+  //tracking if a network provider has being found
   let foundProvider = false;
 
+  //For detecting if a number starts with 0 or +234
+  //if it starts with 0 then the maximum digits of the number will be 11 digits
+  //like wise if it starts with starts otherwise e.g +234 maximun digits is 14 digits
   function detectNumberPattern() {
     if (numCode.startsWith("0")) {
       userInputElement.maxLength = "11";
@@ -25,8 +34,10 @@ userInputElement.oninput = function () {
     }
   }
 
+  //calling the function
   detectNumberPattern();
 
+  //Arrays of all the possible prfixes of the network provider
   let mtn = [
     "0803",
     "0806",
@@ -47,22 +58,22 @@ userInputElement.oninput = function () {
 
   let Nmobile = ["0909", "0908", "0818", "0809", "0817"];
 
+  //function for checking if a number is mtn
   function mtnValidator() {
     for (let i = 0; i < mtn.length; i++) {
       if (numCode.startsWith(mtn[i]) || newIntNumCode.startsWith(mtn[i])) {
-        // feedbackElement.innerText = `The number ${phoneNum} is MTN`;
         networkLogo.src = "./assets/mtn.png";
         userInputElement.style.borderColor = "Yellow";
         networkCarrier.value = "mtn";
-        foundProvider = true;
+        foundProvider = true; // updating the found provider
       }
     }
   }
 
+  //function for checking if a number is glo
   function gloValidator() {
     for (let i = 0; i < glo.length; i++) {
       if (numCode.startsWith(glo[i]) || newIntNumCode.startsWith(glo[i])) {
-        // feedbackElement.innerText = `The number ${phoneNum} is Glo`;
         networkLogo.src = "./assets/glo.jpeg";
         userInputElement.style.borderColor = "Green";
         networkCarrier.value = "glo";
@@ -71,6 +82,7 @@ userInputElement.oninput = function () {
     }
   }
 
+  //function for checking if a number is airtel
   function airtelValidator() {
     for (let i = 0; i < airtel.length; i++) {
       if (
@@ -86,13 +98,13 @@ userInputElement.oninput = function () {
     }
   }
 
+  //function for checking if a number is 9mobile or Nmobile
   function NmobileValidator() {
     for (let i = 0; i < Nmobile.length; i++) {
       if (
         numCode.startsWith(Nmobile[i]) ||
         newIntNumCode.startsWith(Nmobile[i])
       ) {
-        // feedbackElement.innerText = `The number ${phoneNum} is 9mobile`;
         networkLogo.src = "./assets/9mobile.png";
         userInputElement.style.borderColor = "darkgreen";
         networkCarrier.value = "9mobile";
@@ -101,9 +113,10 @@ userInputElement.oninput = function () {
     }
   }
 
+  //conditionals for checking the value of the network provider picked
   switch (networkCarrier.value) {
     case "Detect":
-      selected = true;
+      selected = true; // updating the selected
       detectNumberPattern();
       mtnValidator();
       airtelValidator();
@@ -132,15 +145,18 @@ userInputElement.oninput = function () {
       break;
     default:
       selected = false;
+      //for stopping the user from further inputtting if no network provider picked
       userInputElement.maxLength = "1";
   }
 
+  //if no network picked
   if (!selected) {
-    feedbackElement.innerText = `Please pick a network or pick detect`;
+    feedbackElement.innerText = `Please pick a network or pick detect`; // Display message
   } else {
-    feedbackElement.innerText = "";
+    feedbackElement.innerText = ""; //Don't display message
   }
 
+  //if network selected but no network provider found
   if (selected && !foundProvider) {
     feedbackElement.innerText = `No network provider found`;
     networkLogo.src = "";
@@ -148,20 +164,12 @@ userInputElement.oninput = function () {
   }
 };
 
+//Value of airtime amount user inputted
 let amount = document.getElementById("airtime-amount");
+//Reference to display message
 let alertPurchase = document.getElementById("alertPurchase");
 
-// function validateNumber() {
-//   if (
-//     userInputElement.maxLength != "11" ||
-//     userInputElement.maxLength != "14"
-//   ) {
-//     alertPurchase.style.backgroundColor = "red";
-//     alertPurchase.style.display = "block";
-//     alertPurchase.innerText = "Phone Number not complete";
-//   }
-// }
-
+//message to be displayed when the form is submitted
 document.getElementById("purchaseForm").addEventListener("submit", () => {
   alertPurchase.style.display = "block";
   alertPurchase.style.backgroundColor = "green";
